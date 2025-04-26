d4c1254b6a0672671db39ac6a2fb897eb5b9db89
Ganga Singh <gangasingh1734@gmail.com>
2025-04-27 00:10:51 +0530

    User schema is defined

---
+++ backend/Models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    fullname :{
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minlength : 8
+    },
+    email : {
+        type :String,
+        requird :true,
+        unique : true
+    },
+    fololowers : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        default : []
+    }],
+    following : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        default : []
+    }],
+    profileimg : {
+        type : String,
+        default  : "https://www.svgrepo.com/show/43426/profile.svg"
+    },
+    coverimg : {
+        type  : String,
+        default : "https://www.proactivechannel.com/Files/BrandImages/Default.jpg"
+    },
+    Bio : {
+        type :String,
+        default :""
+    },
+    Link : {
+        type : String,
+        default : ""
+    }
+})
