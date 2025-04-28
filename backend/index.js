f3a1539ee5cc2eeadac50e90661ef0fc201f97fa
Ganga Singh <gangasingh1734@gmail.com>
2025-04-28 14:28:49 +0530

    SignUp Page is Rectified

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,13 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use("/api/auth",authRoutes);
 
 dotenv.config();
-const PORT = process.env.PORT||5000;
 
-app.listen(PORT,()=>{
+
+app.listen(3000,()=>{
     DbConnect();
-    console.log("the server has been started ont the port",PORT)
-})
+    console.log("the server has been started ont the port 3000")
+}) 
