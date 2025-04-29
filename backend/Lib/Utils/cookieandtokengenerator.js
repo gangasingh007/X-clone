a4aadc3b8be55ad8ed06823e012bf4bfe4fabce2
Ganga Singh <gangasingh1734@gmail.com>
2025-04-30 02:14:20 +0530

    Cookie Gernator

--- backend/Lib/Utils/cookieandtokengenerator.js
+++ backend/Lib/Utils/cookieandtokengenerator.js
@@ -9,6 +9,6 @@ export const generateTokenandCookie = async (userId,res) => {
         maxage : "15*24*60*60*1000", //15 days,
         httpOnly : true,
         sameSite : "strict",
-        secure : process.env.NODE_ENV !== development,
+        secure : process.env.NODE_ENV !== "development",
     });
 }
