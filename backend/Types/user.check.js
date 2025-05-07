5b5f6415fcc989640fd2703c4ac8639cdee2ae8c
Ganga Singh <gangasingh1734@gmail.com>
2025-05-08 00:21:22 +0530

    Mores types defined

--- backend/Types/user.check.js
+++ backend/Types/user.check.js
@@ -6,7 +6,7 @@ export const CreateUser = zod.object({
     fullname : zod.string(),
     email : zod.string()
 })
-
+r
 export const validateUser = zod.object({
     username : zod.string(),
     password : zod.string(),
