2f399bcb78b5addfad5462e44e801828a0be4869
Ganga Singh <gangasingh1734@gmail.com>
2025-05-22 23:32:52 +0530

    Commits

--- frontend/src/Components/SignIn.jsx
+++ frontend/src/Components/SignIn.jsx
@@ -15,7 +15,7 @@ const SignIn = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-        },
+        }, 
         body: JSON.stringify({
           username : username,
           password : password
